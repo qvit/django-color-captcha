ecbf2690a0a0e55735bd2068bed49207962a3e22
Victor Smirnov <qvit@HELLCODE.(none)>
2013-08-21 00:12:49 +0600

    Fixed DEFAULT_COLORS setting

--- color_captcha/settings.py
+++ color_captcha/settings.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
-from django.utils.translation import ugettext_lazy as _
 from utils import check_colors
 
 
 DEFAULT_COLORS = [
-    (u'white', _('white')),
-    (u'blue', _('blue')),
-    (u'red', _('red')),
+    ('white', 'white'),
+    ('blue', 'blue'),
+    ('red', 'red'),
 ]
 
 COLORS = getattr(settings, 'CAPTCHA_COLORS', DEFAULT_COLORS)
